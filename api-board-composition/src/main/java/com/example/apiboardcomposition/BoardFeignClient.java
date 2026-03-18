d8eb36516d70d7843fe749061a79ec17c3a1845d
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-03-18 14:04:18 +0900

    [Feat] CircuitBreaker 패턴

--- api-board-composition/src/main/java/com/example/apiboardcomposition/BoardFeignClient.java
+++ api-board-composition/src/main/java/com/example/apiboardcomposition/BoardFeignClient.java
@@ -1,11 +1,14 @@
 package com.example.apiboardcomposition;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="boardFeignClient", url="http://localhost:8081")
+@FeignClient(name="boardFeignClient", url="http://localhost:8081", fallbackFactory = BoardFeignClientFallbackFactory.class)
 public interface BoardFeignClient {
+
     @GetMapping("/board/command/{idx}")
     public BoardDto.BoardRes getBoard(@PathVariable Long idx);
+
 }
